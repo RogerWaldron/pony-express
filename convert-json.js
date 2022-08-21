f80f93d004a25926225664c82408a1e9b66142b5
Roger Waldron <roger.waldron@gmail.com>
2022-08-21 17:22:06 +0200

    refactor: :sparkles: json returned in format according to Accepts header
    Accepted formats are json, xml, and csv. If no header then json is returned by default

---
+++ convert-json.js
@@ -0,0 +1,9 @@
+module.exports = {
+  jsonToCsv(data) {
+    let csv = data.map((row) =>
+      Object.values(row).map((item) => JSON.stringify(item))
+    );
+    csv.unshift(Object.keys(data[0])); // header row
+    return csv.join("\n");
+  },
+};
--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-const http = require("http");
+const express = require("express");
+const convert = require("./convert-json.js");
+let builder = require("xmlbuilder");
 
 const users = require("./fixtures/users");
 const emails = require("./fixtures/emails");
+const { jsonToCsv } = require("./convert-json.js");
 
-let server = http.createServer((req, res) => {
+let app = express();
+
+app.use((req, res) => {
   let route = req.method + " " + req.url;
+  const prefResp = req.accepts([
+    "application/json",
+    "application/xml",
+    "text/csv",
+  ]);
+  res.type(prefResp || "json");
 
   if (route === "GET /users") {
-    res.end(JSON.stringify(users));
+    switch (prefResp) {
+      case "text/csv":
+        res.send(convert.jsonToCsv(users));
+        break;
+      case "application/xml":
+        let xml = builder.create("users");
+        users.map((item) => xml.ele({ user: item }));
+        res.send(xml.end({ pretty: true }));
+        break;
+      case "application/json":
+      default:
+        res.send(users);
+        break;
+    }
   } else if (route === "GET /emails") {
-    res.end(JSON.stringify(emails));
+    switch (prefResp) {
+      case "text/csv":
+        res.send(convert.jsonToCsv(emails));
+        break;
+      case "application/xml":
+        let xml = builder.create("emails");
+        emails.map((item) => xml.ele({ email: item }));
+        res.send(xml.end({ pretty: true }));
+        break;
+      case "application/json":
+      default:
+        res.send(emails);
+        break;
+    }
   } else {
     res.end("You asked for " + route);
   }
 });
 
-server.listen(3000);
+app.listen(3000);
