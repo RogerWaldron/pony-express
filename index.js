19fe2270f2faa89451bbacbf2103688e34d6a94c
Roger Waldron <roger.waldron@gmail.com>
2022-08-22 12:14:35 +0200

    feat: Added ServeStatic middleware and public website frontend skeleton

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const path = require("path");
 
 const logger = require("./lib/logger");
 const usersRouter = require("./routes/users");
 const emailsRouter = require("./routes/emails");
 const compress = require("compression");
+const serveStatic = require("serve-static");
 
 let app = express();
 
 app.use(logger);
-app.use(compress())
+app.use(compress());
 app.use("/users", usersRouter);
 app.use("/emails", emailsRouter);
+app.use(serveStatic(path.join(__dirname, "public")));
 
 app.listen(3000);
